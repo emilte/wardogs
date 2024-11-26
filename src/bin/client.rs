e4768e8fe95ac71a6ae4c26e0fc6e92453b8e1dc
Emil Telstad <22004178+emilte@users.noreply.github.com>
2024-11-26 12:08:01 +0100

    Copy curve-fever.

---
+++ src/bin/client.rs
@@ -0,0 +1,89 @@
+use bevy::prelude::*;
+use bevy_renet::{
+    renet::{
+        transport::{ClientAuthentication, NetcodeClientTransport},
+        ConnectionConfig, DefaultChannel, RenetClient,
+    },
+    transport::NetcodeClientPlugin,
+    RenetClientPlugin,
+};
+use curve_fever::{bullet, physics, player, MultiplayerEvent, MyClientId, PROTOCOL_ID};
+use std::{net::UdpSocket, time::SystemTime};
+
+fn main() {
+    let (client_id, client, transport) = new_renet_client();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(RenetClientPlugin)
+        .add_plugins(NetcodeClientPlugin)
+        .add_systems(PreUpdate, bullet::player_shooting_system)
+        .add_systems(Startup, setup_game)
+        .add_systems(
+            Update,
+            (
+                physics::friction_system,
+                physics::velocity_system,
+                player::control_player_system,
+                // player::crazy_player_particle_system,
+                player::face_in_direction_system,
+                player::lifetime_system,
+                player::spawn_player_system,
+                player::sync_player_with_server_system,
+                player::sync_other_player_positions_system,
+                receive_message_system,
+                receive_reliable_message_system,
+            ),
+        )
+        .insert_resource(client)
+        .insert_resource(transport)
+        .insert_resource(MyClientId(client_id))
+        .add_event::<MultiplayerEvent>()
+        .run();
+}
+
+fn new_renet_client() -> (u64, RenetClient, NetcodeClientTransport) {
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    let client_id = rand::random();
+
+    let authentication = ClientAuthentication::Unsecure {
+        client_id,
+        protocol_id: PROTOCOL_ID,
+        server_addr,
+        user_data: None,
+    };
+
+    let current_time = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap();
+
+    let client = RenetClient::new(ConnectionConfig::default());
+    let transport = NetcodeClientTransport::new(current_time, authentication, socket).unwrap();
+
+    (client_id, client, transport)
+}
+
+fn setup_game(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    println!("Started Game.");
+}
+
+fn receive_message_system(
+    mut client: ResMut<RenetClient>,
+    mut events: EventWriter<MultiplayerEvent>,
+) {
+    while let Some(msg) = client.receive_message(DefaultChannel::Unreliable) {
+        let message = bincode::deserialize(&msg).expect("Failed to deserialize message");
+        events.send(message);
+    }
+}
+
+fn receive_reliable_message_system(
+    mut client: ResMut<RenetClient>,
+    mut events: EventWriter<MultiplayerEvent>,
+) {
+    while let Some(msg) = client.receive_message(DefaultChannel::ReliableUnordered) {
+        let message = bincode::deserialize(&msg).expect("Failed to deserialize message");
+        events.send(message);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use bevy::prelude::*;
+use bevy_renet::renet::Bytes;
+use serde::*;
+
+pub mod bullet;
+pub mod physics;
+pub mod player;
+
+/// Protocol ID needs to be the same across client/server.
+pub const PROTOCOL_ID: u64 = 0;
+
+#[derive(Resource)]
+pub struct MyClientId(pub u64);
+
+#[derive(Debug, Serialize, Deserialize, Event)]
+pub enum MultiplayerEvent {
+    Ping { hello: String },
+    PlayerCreated(ServerPlayer),
+    PlayerMoved(ServerPlayer),
+    PlayerDeleted(u64),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ServerPlayer {
+    pub client_id: u64,
+    pub position: Vec2,
+}
+
+impl MultiplayerEvent {
+    pub fn bytes(self) -> Bytes {
+        bincode::serialize(&self)
+            .expect("Failed to serialize multiplayer event")
+            .into()
+    }
+}
